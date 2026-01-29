31c411125b3d379ba28c68268cb26dca1a5e5deb
JACOB <samadeus@gmail.com>
2026-01-29 05:46:31 +0100

    Balance home page layout and final sidebar logout fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -19,7 +19,7 @@ import {
 } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import { useState, useEffect } from 'react';
-
+export const dynamic = 'force-dynamic';
 export default function DashboardLayout({
   children,
 }: {
